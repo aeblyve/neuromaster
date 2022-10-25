b57b3c299bbc8c6b650e1a1863c9ecb4b57d679e
Leonid Belyaev <belyaev.l@northeastern.edu>
2022-10-25 17:47:42 -0400

    rendered edges

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use kiss3d::scene::SceneNode;
 use macroquad::prelude::*;
+use nalgebra::Point3;
+use petgraph::graph::NodeIndex;
+use petgraph::stable_graph::EdgeIndex;
 use rust_nmap::parse_nmap_xml_bytes;
 use std::collections::HashMap;
 
@@ -12,14 +15,15 @@ mod simulation;
 fn main() {
     let full_parse = parse_nmap_xml_bytes(include_bytes!("../assets/scan.xml")).unwrap();
     let mut simulation = simulation::build_simulation(full_parse).unwrap();
-    let mut scene_map = HashMap::<String, SceneNode>::new();
+    let mut node_map = HashMap::<NodeIndex, SceneNode>::new();
 
     let mut window = Window::new("Neuroquad");
     window.set_light(Light::StickToCamera);
 
-    for node in simulation.get_graph().node_weights() {
+    // build nodes
+    for node_index in simulation.get_graph().node_indices() {
         let scene_node = wireframe_sphere(&mut window);
-        scene_map.insert(node.name.clone(), scene_node);
+        node_map.insert(node_index, scene_node);
     }
 
     // saves resources AND looks very "Neuromancer"
@@ -34,11 +38,33 @@ fn main() {
 
     while window.render() {
         simulation.update(0.035);
-        for node in simulation.get_graph().node_weights() {
-            let scene_node = scene_map.get_mut(&node.name).unwrap();
-            let translation =
-                nalgebra::Translation3::new(node.location.x, node.location.y, node.location.z);
+        let graph = simulation.get_graph();
+        for node_index in graph.node_indices() {
+            let node_weight = graph.node_weight(node_index).unwrap();
+            let scene_node = node_map.get_mut(&node_index).unwrap();
+            let translation = nalgebra::Translation3::new(
+                node_weight.location.x,
+                node_weight.location.y,
+                node_weight.location.z,
+            );
             scene_node.set_local_translation(translation);
+
+            for neighbor_index in graph.neighbors(node_index) {
+                let neighbor_weight = &graph.node_weight(neighbor_index).unwrap();
+                window.draw_line(
+                    &Point3::new(
+                        node_weight.location.x,
+                        node_weight.location.y,
+                        node_weight.location.z,
+                    ),
+                    &Point3::new(
+                        neighbor_weight.location.x,
+                        neighbor_weight.location.y,
+                        neighbor_weight.location.z,
+                    ),
+                    &Point3::new(0.0, 1.0, 0.0),
+                );
+            }
         }
     }
 }
