92fbe4d1722ca3ccf84ba953b4c9b8388a313698
Anonymous <>
2023-02-21 14:09:26 -0500

    nixOS stuff

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  inputs = {
+    naersk.url = "github:nmattia/naersk/master";
+    # This must be the stable nixpkgs if you're running the app on a
+    # stable NixOS install.  Mixing EGL library versions doesn't work.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-mozilla, utils, naersk, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system;
+	  overlays = [
+	    (import nixpkgs-mozilla)
+	  ];
+	};
+	toolchain = (pkgs.rustChannelOf {
+	  rustToolchain = ./rust-toolchain.toml;
+	  sha256 = "";
+	}).rust;
+        naersk-lib = pkgs.callPackage naersk { 
+	  cargo = toolchain;
+	  rustc = toolchain;
+	};
+        libPath = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          doCheck = true;
+          pname = "neuromaster";
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = with pkgs; [
+            xorg.libxcb
+          ];
+          postInstall = ''
+            wrapProgram "$out/bin/neuromaster" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
+        };
+
+        defaultApp = utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
+        };
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            cargo
+            cargo-insta
+            pre-commit
+            rust-analyzer
+            rustPackages.clippy
+            rustc
+            rustfmt
+            tokei
+
+            xorg.libxcb
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = libPath;
+          GIT_EXTERNAL_DIFF = "${difftastic}/bin/difft";
+        };
+      });
+}
+
+
